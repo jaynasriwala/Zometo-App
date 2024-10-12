1d934e722e6823b3261b879e86d475ec2423c835
Jay Nasriwala <jaynasriwala263@gmail.com>
2024-10-12 10:51:35 +0530

    menu crud

--- src/main/java/com/controller/MenuController.java
+++ src/main/java/com/controller/MenuController.java
@@ -1,8 +1,66 @@
 package com.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MenuController {
+import com.entity.MenuEntity;
+import com.repository.MenuRepository;
 
+@RestController
+public class MenuController 
+{
+	
+	@Autowired
+	MenuRepository menuRepository;
+
+	//add menu
+	@PostMapping("/addmenu")
+	public MenuEntity addMenu(@RequestBody MenuEntity menuEntity)
+	{
+		menuRepository.save(menuEntity);
+		
+		return menuEntity;
+	}
+	
+	//get menu
+	
+	//problem in postman
+	@GetMapping("/getmenubyid/{id}")
+	public MenuEntity getMenuById(@PathVariable("id") Integer id)
+	{
+		
+		Optional<MenuEntity> op =  menuRepository.findById(id);
+		
+		if(op.isEmpty())
+		{
+			return null;
+		}
+		else
+		{
+			return op.get();
+		}
+	}
+	
+	// delete menu
+	@DeleteMapping("/deletebyid/{id}")
+	public String deleteById(@PathVariable("id") Integer id) {
+		menuRepository.deleteById(id);
+		return "SuccessFully Deleted Record...";
+	}
+	
+	
+	//update menu
+	@PutMapping("/updatemenu")
+	public MenuEntity updateMenu(@RequestBody MenuEntity menuEntity) {
+		menuRepository.save(menuEntity);
+		return menuEntity;
+	}
 }
--- src/main/java/com/entity/MenuEntity.java
+++ src/main/java/com/entity/MenuEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -22,4 +24,7 @@ public class MenuEntity
 	String title;
 	String menuImagePath;
 
+	@ManyToOne
+	@JoinColumn(name="restaurantId")
+	RestaurantEntity restaurant;
 }
